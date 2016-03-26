74694de0cf53f9a245c3c90cbaaaace2b98131c6
absk432 <rsrinandan@gmail.com>
2016-03-26 22:27:11 +0530

    Update maze.java

--- maze.java
+++ maze.java
@@ -6,15 +6,32 @@ public class maze
 	public static void main(String args[]) throws FileNotFoundException
 	{
 		
-		 int[][] matrix= new int[15][5];
-
+		 
+		int[][] matrix;
 	
 		 try
 			{
+			 
+		
+			BufferedReader f0= new BufferedReader(new FileReader("CreateMaze.txt"));
+			
+			Scanner sc =new Scanner(f0);
+		
+			int linecount=0;
+			String cline = null;
+			while((cline = f0.readLine())!=null)
+			{
+				linecount+=1;
+				
+			}
+			f0.close();
+			 
+			matrix= new int[linecount][5];
+			 
 			BufferedReader f1= new BufferedReader(new FileReader("CreateMaze.txt"));
 			Scanner in =new Scanner(f1);
-			//String row;
-				//int[][] matrix= new int[15][5];
+			
+			
 			int[] l1= new int[5];
 			
 			String line = null;
@@ -24,26 +41,19 @@ public class maze
 				int j=0;
 				int k=0;
 				int g=0;
-				//row=in.nextLine();
+				
 				String h[]=line.split(" ");
-				//System.out.println(line);
+				
 				while(j<5 )
 				{
 				
 					matrix[i][j]=Integer.parseInt(h[g++]);
 					j=j+1;
-					//k=k+2;
+					
 				}
 				i=i+1;
 			}
-			/*for(int z=0;z<15;z++)
-			{
-				for(int j=0;j<5;j++)
-				{
-					System.out.print(matrix[z][j]+" ");
-				}
-				System.out.println();
-			}*/
+			
 			
 		 BufferedReader f2= new BufferedReader(new FileReader("TraverseMaze.txt"));
 		Scanner on =new Scanner(f2);
@@ -51,20 +61,21 @@ public class maze
 		BufferedWriter writer = 
 			    new BufferedWriter(new FileWriter("Destinations.txt"));
 	
-
+		
 		int currentRoom=0;
 		String line1 = null;
 		while((line1 = f2.readLine())!=null)
 		{
-
+			
 			int y=0;
 			int hh=1;
 			String[] kk=line1.split(" ");
 			int roomNumber=Integer.parseInt(kk[y++]);
 			int jj=kk.length;
-			boolean flag=true;
+			
 			while(hh<jj)
 			{
+				
 				hh++;
 				
 				int n=Integer.parseInt(kk[y++]);
@@ -80,6 +91,7 @@ public class maze
 			writer.newLine();
 
 		}
+		
 		
 		
 		writer.close();
